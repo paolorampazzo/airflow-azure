536a2afe508592639c897f26e55d7e970baf32f2
paolo.rampazzo <paolo.rampazzo@datarisk.io>
2023-10-04 17:10:29 -0300

    Teste

--- airflow-azure/dags/oracle.py
+++ airflow-azure/dags/oracle.py
@@ -73,8 +73,7 @@ with DAG(
     # [START howto_operator_python]
     # @task(task_id="print_the_context",
     #       executor_config=define_k8s_specs(**get_specifications_from_ui_params(dag.params), other_specs={}))
-    @task(task_id="print_the_context",
-          executor_config=define_k8s_specs(memory_request='2Gi', other_specs={}))
+    @task(task_id="print_the_context")
     def print_context(ds=None, **kwargs):
         """Print the Airflow context and ds variable from the context."""
         # pprint(kwargs)
