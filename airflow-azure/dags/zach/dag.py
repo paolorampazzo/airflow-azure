ce93335aaa92486b6249e83afac85bac37686bd0
paolo.rampazzo <paolo.rampazzo@datarisk.io>
2024-01-11 01:47:27 -0300

    teste

--- airflow-azure/dags/zach/dag.py
+++ airflow-azure/dags/zach/dag.py
@@ -20,28 +20,9 @@ with DAG(dag_id="download_videos",
 ) as dag:
     
     @task
-    def read_file_content():
-    # Read the content of your file
-        file_content = ""
-        with open("pvc.yaml", "r") as file:
-            file_content = file.read()
-        return {'file_content': file_content}
-
-    create_pvc_task = KubernetesPodOperator(
-    task_id='create_pvc_task',
-    name='create-pvc',
-    # namespace='your_namespace',
-    image='paolorampazzodatarisk/dockerkubectl:1.0',
-    cmds=['sh', '-c'],
-    # arguments=['kubectl apply -f /opt/airflow/dags/repo/airflow-azure/dags/zac/pvc.yaml'],
-    arguments=['echo {{task_instance.xcom_pull(task_ids="read_file_content", key="file_content")}};sleep infinity'],
-    # arguments=['kubectl apply -f pvc.yaml'],
-    # volumes=[k8s.V1Volume(name='dags')],
-    # volume_mounts=[k8s.V1VolumeMount(name='dags', mount_path = '/opt/airflow/dags/')],
-    get_logs=True,
-    dag=dag,
-)
-
+    def kubectl():
+        import subprocess
+        subprocess.run(["kubectl", "apply", "-f", "pvc.yaml"])
 
     @task
     def get_jwt(jwt: str):
@@ -65,4 +46,4 @@ with DAG(dag_id="download_videos",
     added_values = add_one.expand(x=[1, 2, 3])
     sum_it(added_values)
 
-    create_pvc_task = read_file_content()
+    kubectl()
