d2b061dafca9ff3c8f366602ad9970aa43716629
paolo.rampazzo <paolo.rampazzo@datarisk.io>
2024-01-12 18:38:17 -0300

    teste

--- airflow-azure/dags/zach/download_course.py
+++ airflow-azure/dags/zach/download_course.py
@@ -79,5 +79,5 @@ with DAG(dag_id="download_course",
         print(content)
 
     metadata = get_metadata()
-    download_file.expand(metadata = [{**metadata, **{'index': k for k in range(metadata['max_index'])}}])
+    download_file.partial().expand(metadata = [{**metadata, **{'index': k for k in range(metadata['max_index'])}}])
 
